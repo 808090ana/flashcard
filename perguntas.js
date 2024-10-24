b5dacbd9d982c84dbf232099a14993dad33a9710
808090ana <138120919+808090ana@users.noreply.github.com>
2024-10-24 10:47:11 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é programação?',
+    'É a arte de escrever instruções para computadores para que eles possam realizar tarefas e executar instruções específicas.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Canadá?',
+    'A capital da França é Ottawa'
+)
+
+criaCartao(
+    'Programação',
+    'O que é a declaração de DOCTYPE em HTML?',
+    'É a primeira linha de código necessária em qualquer documento, HTML ou XHTML.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz amo você em Inglês?',
+    'Oi em ingles é love you'
+)
